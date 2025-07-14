0a39d2b78659d86bdeae757b79910662dd679f8c
David Valero <nestordavidvalero@gmail.com>
2025-07-14 13:31:04 -0500

    :boom: Validating

--- howtouse/azure/backend/backend.tf
+++ howtouse/azure/backend/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-	storage_account_name  = "terraform"
+	storage_account_name  = "terraformjefferson"
 	container_name        = "tfstatejeff"
 	key                   = "terraform.tfstatejeff"
-    snapshot              = true
+  snapshot              = true
   }
 }
