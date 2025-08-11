5c82a734c587e44f97dbf0d0d52fb42aa2c6896b
Jeff <jefferson.soto@outlook.com>
2025-08-11 11:12:06 -0500

    feat: add support for multiple Azure regions for resource group creation

--- modules/azure/resource-group/main.tf
+++ modules/azure/resource-group/main.tf
@@ -3,4 +3,5 @@ resource "azurerm_resource_group" "main_for_each" {
   name     = each.key # Use the key as the name of the resource group
   location = each.value.location
   tags     = try(each.value.tags, null)
+  
 }
