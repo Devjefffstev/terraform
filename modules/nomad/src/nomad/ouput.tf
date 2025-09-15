dd68933b18725e5f96700fa4e1b0b0bd1337d078
Jefferson Soto <jeffersonsoto@Jeffersons-MacBook-Air.local>
2025-09-15 10:42:25 -0500

    update repo

--- modules/nomad/src/nomad/ouput.tf
+++
@@ -1,9 +0,0 @@
-output "nomad-server-vm" {
-  value     = module.nomad-client.resource
-  sensitive = true
-}
-
-output "nomad-client-vm" {
-  value     = module.nomad-client.resource
-  sensitive = true
-}
