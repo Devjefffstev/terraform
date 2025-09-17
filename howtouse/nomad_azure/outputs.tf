47b39c0215c702397d11862061266715591c1cc2
Jefferson Soto <jeffersonsoto@Jeffersons-MacBook-Air.local>
2025-09-17 15:13:28 -0500

    add outputs nic

--- howtouse/nomad_azure/outputs.tf
+++ howtouse/nomad_azure/outputs.tf
@@ -3,27 +3,31 @@ output "private_key" {
   sensitive = true
 }
 
-output "nomad_ip" {
-  value = "http://${module.nomad_cluster_client.module.avm_res_compute_virtualmachine[0].public_ip_address}:4646/ui"
+output "vm_mod_network_interfaces_root" {
+  value = module.nomad_cluster_client.vm_mod_network_interfaces
 }
 
-output "IP_Addresses" {
-  value = <<CONFIGURATION
+# output "nomad_ip" {
+#   value = "http://${module.nomad_cluster_client.module.avm_res_compute_virtualmachine[0].public_ip_address}:4646/ui"
+# }
 
-It will take a little bit for setup to complete and the UI to become available.
-Once it is, you can access the Nomad UI at:
+# output "IP_Addresses" {
+#   value = <<CONFIGURATION
 
-http://${module.nomad_cluster_client.module.avm_res_compute_virtualmachine[0].public_ip_address}:4646/ui
+# It will take a little bit for setup to complete and the UI to become available.
+# Once it is, you can access the Nomad UI at:
 
-Set the Nomad address, run the bootstrap, export the management token, set the token variable, and test connectivity:
+# http://${module.nomad_cluster_client.module.avm_res_compute_virtualmachine[0].public_ip_address}:4646/ui
 
-export NOMAD_ADDR=http://${module.nomad_cluster_client.module.avm_res_compute_virtualmachine[0].public_ip_address}:4646 && \
-nomad acl bootstrap | grep -i secret | awk -F "=" '{print $2}' | xargs > nomad-management.token && \
-export NOMAD_TOKEN=$(cat nomad-management.token) && \
-nomad server members
+# Set the Nomad address, run the bootstrap, export the management token, set the token variable, and test connectivity:
 
-Copy the token value and use it to log in to the UI:
+# export NOMAD_ADDR=http://${module.nomad_cluster_client.module.avm_res_compute_virtualmachine[0].public_ip_address}:4646 && \
+# nomad acl bootstrap | grep -i secret | awk -F "=" '{print $2}' | xargs > nomad-management.token && \
+# export NOMAD_TOKEN=$(cat nomad-management.token) && \
+# nomad server members
 
-cat nomad-management.token
-CONFIGURATION
-}
+# Copy the token value and use it to log in to the UI:
+
+# cat nomad-management.token
+# CONFIGURATION
+# }
