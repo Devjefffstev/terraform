54893b035bcfdff4780210808e4d6a77b83f2037
Jefferson Soto <jeffersonsoto@Jeffersons-MacBook-Air.local>
2025-09-30 19:29:17 -0500

    fix(rg): remove docs

--- modules/azure/resource-group-single/main.tf
+++ modules/azure/resource-group-single/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "main_single" {
-  name = var.rg_prop_name_single
+  name     = var.rg_prop_name_single
   location = var.rg_prop_location_single
-  tags = try(var.rg_prop_tags_single, null)
+  tags     = try(var.rg_prop_tags_single, null)
 }
--- modules/azure/resource-group-single/outputs.tf
+++ modules/azure/resource-group-single/outputs.tf
@@ -1,4 +1,4 @@
 output "resource_group_prop_single" {
   description = "All resource group properties"
-  value       = azurerm_resource_group.main_single  
+  value       = azurerm_resource_group.main_single
 }
