a7ce3cebae7423d6a3df41496bd67d2efbc58408
Jefferson Soto <jeffersonsoto@Jeffersons-MacBook-Air.local>
2025-09-30 19:43:34 -0500

    space

--- modules/azure/policy_initiative/main.tf
+++ modules/azure/policy_initiative/main.tf
@@ -229,7 +229,6 @@ resource "azurerm_resource_policy_exemption" "resource_exemptions" {
     azurerm_management_group_policy_assignment.policy_assignment[0].id) : (
     azurerm_subscription_policy_assignment.policy_assignment[0].id
   )
-
   expires_on                      = each.value.expires_on
   policy_definition_reference_ids = each.value.policy_definition_reference_ids
   metadata                        = each.value.metadata
