256684d3937aa7c4dae857ea072dee9aef1963a0
Jefferson Soto <jeffersonsoto@Jeffersons-MacBook-Air.local>
2025-10-01 15:08:33 -0500

    add folder for apim

---
+++ howtouse/azure/apim/main.tf
@@ -0,0 +1,6 @@
+## AVM modules for Azure API Management (APIM)
+module "avm-res-apimanagement-service" {
+  source  = "Azure/avm-res-apimanagement-service/azurerm"
+  version = "0.0.5"
+  # insert the 4 required variables here
+}
