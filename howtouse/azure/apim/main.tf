0720faedd26916cc22173981e4236470ff0bb7fe
Jefferson Soto <jeffersonsoto@Jeffersons-MacBook-Air.local>
2025-10-01 15:26:55 -0500

    change for a script

--- howtouse/azure/apim/main.tf
+++ howtouse/azure/apim/main.tf
@@ -3,4 +3,9 @@ module "avm-res-apimanagement-service" {
   source  = "Azure/avm-res-apimanagement-service/azurerm"
   version = "0.0.5"
   # insert the 4 required variables here
+  name= "example-apim"
+    location = "West Europe"
+    resource_group_name = "example-resources"
+    publisher_name = "example-publisher"
+    publisher_email = "example@example.com"
 }
